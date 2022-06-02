fcf30eefdd8ba69091c749d4122d67bddd88371f
Daniela Farias <farias.danicris@gmail.com>
2022-06-02 00:37:22 -0300

    Login e Dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,33 @@
-import type { NextPage } from 'next'
+import {
+  Button,
+  Flex,
+  Stack,
+} from "@chakra-ui/react";
+import type { NextPage } from "next";
+import { Input } from "../components/Form/Input";
 
-const Home: NextPage = () => {
+const SignIn: NextPage = () => {
   return (
-    <h1>
-      DASHGO
-    </h1>
-  )
-}
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+         <Input type="email" name="email" label="E-mail" />
+         <Input type="password" name="password" label="Senha" />
+        </Stack>
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
+};
 
-export default Home
+export default SignIn;
